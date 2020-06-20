05da2e89493991bc81317b46f25e6971afd8d334
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-06-20 14:11:29 +0300

    finish ()

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const btn = document.querySelector('#btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  //get random number between 0-3
+  const randomNumber = getRandomeNumber();
+
+  document.body.style.background = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomeNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,21 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const btn = document.querySelector('#btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  let hexColor = '#';
+
+  //get randome ele from hex array and added to the hexColor variable
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomeNumber()];
+    // console.log(hexColor);
+  }
+
+  document.body.style.background = hexColor;
+  color.textContent = hexColor;
+});
+
+function getRandomeNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
